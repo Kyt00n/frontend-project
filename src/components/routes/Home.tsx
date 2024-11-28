3c1c66f1e414785239702dce3ebcd43011575be6
Kytoon <eryk.olsza@gmail.com>
2024-11-28 01:08:00 +0100

    Authentication front initially done

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,10 +1,24 @@
-import { FC } from "react"
+import { FC, useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import UserService from "../../services/user.service";
 
 const Home: FC = () => {
+    const [content, setContent] = useState<string>("");
+    useEffect(() => {
+        UserService.getPublicContent().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                setContent(_content);
+            }
+        );
+    }, []);
     return (
         <div>
             Home Component
+            {content}
             <Link to="/gallery">Galeria</Link>
         </div>
     );
--- src/components/routes/Navigation.tsx
+++ src/components/routes/Navigation.tsx
@@ -1,13 +1,42 @@
-import { FC } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from './Home';
 import Gallery from "./Gallery";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { logout } from "../../actions/auth.action";
+import Login from "../authentication/Login";
+import Register from "../authentication/Register";
+import Profile from "./Profile";
 
 const Navigation: FC = () => {
+    const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+    const [showAdminBoard, setShowAdminBoard] = useState(false);
+
+    const { user: currentUser } = useSelector((state:RootState) => state.auth);
+    const dispatch = useDispatch<AppDispatch>();
+
+    const logOut = useCallback(() => {
+        dispatch(logout());
+      }, [dispatch]);
+
+    useEffect(() => {
+        if (currentUser) {
+          setShowModeratorBoard(currentUser.roles.includes("ROLE_MODERATOR"));
+          setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
+        } else {
+          setShowModeratorBoard(false);
+          setShowAdminBoard(false);
+        }
+      }, [currentUser]);
+    
     return (
         <Routes>
             <Route index element={<Home />}/>
             <Route path="gallery" element={<Gallery />}/>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="profile" element={<Profile />} />
         </Routes>
     );
 }
